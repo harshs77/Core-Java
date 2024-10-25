ed95542c7a031ec7828feabe02b065c44f22f0fa
harshs77 <harshchauhan77423@gmail.com>
2024-10-25 14:15:41 +0530

    Basic Assignment of java

---
+++ Assignment/Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+    public static void main(String[] args) {
+       float a=8.9f,b=1.67f,c=6.21f,d=5.6f,e=7.8f,f=34,g=157,sum;
+       sum= a*b-(c-d)+e-f +g;
+       System.out.println("The sum is :" + sum);
+    }   
+   }
